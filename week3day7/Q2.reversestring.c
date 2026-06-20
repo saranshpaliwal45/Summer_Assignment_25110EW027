475295b158f53e674265ead8c88c7fea9e0a0616
saranshpaliwal45 <saranshpaliwal7@gmail.com>
2026-06-20 22:56:34 +0530

    Add files via upload

---
+++ week3day7/Q2.reversestring.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int len = 0, i;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while(str[len] != '\0')
+    {
+        len++;
+    }
+
+    printf("Reversed String: ");
+
+    for(i = len - 1; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
