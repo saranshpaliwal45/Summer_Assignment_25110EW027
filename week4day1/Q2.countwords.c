f741a08a0e57538761bddce1dddce97c430446db
saranshpaliwal45 <saranshpaliwal7@gmail.com>
2026-06-21 23:55:16 +0530

    Add files via upload

---
+++ week4day1/Q2.countwords.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[200];
+    int i, count = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ' && str[i+1] != ' ')
+            count++;
+    }
+
+    printf("Total words = %d", count);
+
+    return 0;
+}
